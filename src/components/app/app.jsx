9ccfcc4753c5d70c3a121f1a4dc0ca0ea7be3b6d
Artem Tulupov <artemtu94@gmail.com>
2023-06-30 20:06:32 +0400

    feature: header tets with random text & styles

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,48 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import Header from "../header/header";
 
 function App() {
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Изменил
-      </pre>
+      <Header />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import styles from "./app.module.css";
+// import { data } from "../../utils/data";
+
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <pre style={{
+//       	margin: "auto",
+//       	fontSize: "1.5rem"
+//       }}>
+//       	Изменил
+//       </pre>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import text from './header.css'
+
+function Header() {
+  return (
+    <h1 className='text'> это header</h1>
+  )
+}
+
+export default Header
