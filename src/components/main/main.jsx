621267fdaefcaf5e1fd75d78881195e19e7c8a19
Artem Tulupov <artemtu94@gmail.com>
2023-07-04 15:18:55 +0400

    feature: add tabmenu with styles

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import content from "./main.css";
-
-
-
+import { TabMenu } from "../tab/tab";
 function Main() {
   return (
     <main className="content">
       <section className="menu-bar mt-10 ">
         <h1 className="text text_type_main-large">Соберите бургер</h1>
+        <TabMenu />
       </section>
-
-
 
       {/* вторая часть страницы */}
-      <section className="constructor-bar">
-      </section>
+      <section className="constructor-bar"></section>
     </main>
   );
 }
