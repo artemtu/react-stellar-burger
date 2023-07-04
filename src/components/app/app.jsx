904ded9cf2ba5555ed06a94c48373da8a3150558
Artem Tulupov <artemtu94@gmail.com>
2023-07-04 13:31:55 +0400

    feature: add main, set up style

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,14 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import Header from "../header/header";
+import Main from "../main/main";
+
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Main/>
     </div>
   );
 }
---
+++ src/components/main/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import content from "./main.css";
+
+function Main() {
+  return (
+    <main className="content">
+      <section className="menu-bar">
+        <h1>Соберите бургер</h1>
+      </section>
+      <section className="constructor-bar">
+        <h1>Бургер собери ё</h1>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
