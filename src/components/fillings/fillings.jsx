76b631ddd6213cf9c54c015e608784abd62e0396
Artem Tulupov <artemtu94@gmail.com>
2023-07-04 17:26:57 +0400

    feature: add a new comp Fillings with h1

---
+++ src/components/fillings/fillings.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function Fillings() {
+  return (
+    <div>
+        <h2 className='text text_type_main-medium mt-10'>Булки</h2>
+
+
+
+    </div>
+  )
+}
+
+export default Fillings
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import content from "./main.css";
 import { TabMenu } from "../tab/tab";
+import Fillings from "../fillings/fillings";
 function Main() {
   return (
     <main className="content">
       <section className="menu-bar mt-10 ">
         <h1 className="text text_type_main-large">Соберите бургер</h1>
         <TabMenu />
+        <Fillings/>
+        
       </section>
 
       {/* вторая часть страницы */}
