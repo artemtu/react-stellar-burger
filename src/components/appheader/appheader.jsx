fea57b3592b02c3ed2cd11640f8d4ff3371bf85b
Artem Tulupov <artemtu94@gmail.com>
2023-07-05 17:00:57 +0400

    feature: header logo on the center (temporary sol)

--- src/components/appheader/appheader.jsx
+++ src/components/appheader/appheader.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import text from './appheader.css'
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
-import {BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components'
-import {ListIcon} from '@ya.praktikum/react-developer-burger-ui-components'
-import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import React from "react";
+import text from "./appheader.css";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function Header() {
   return (
-    <header className='header p-4'>
-        <a href='#' className='header__link pl-5 pr-5'>
-        <BurgerIcon type="primary"  />
-        <p className='text text_type_main-default pl-2 pr-2' > Конструктор</p>
+    <header className="header p-4">
+      <div className="test">
+        <a href="#" className="header__link pl-5 pr-5">
+          <BurgerIcon type="primary" />
+          <p className="text text_type_main-default pl-2 pr-2"> Конструктор</p>
         </a>
-        <a href="#"  className='header__link ml-2 pl-5 pr-5'>
-        <ListIcon type="secondary" />
-        <p className='text text_type_main-default text_color_inactive pr-2 pl-2' > Лента заказов</p>
+        <a href="#" className="header__link ml-2 pl-5 pr-5">
+          <ListIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive pr-2 pl-2">
+            {" "}
+            Лента заказов
+          </p>
         </a>
-        <Logo/>
-        <a href="#" className='header__link pl-5 pr-5'>
-        <ProfileIcon type="secondary" />
-        <p className='text text_type_main-default text_color_inactive pr-2 pl-2' > Личный кабинет</p>
-        </a>
-    </header>
-   
+      </div>
 
-  )
+      <div className="lel">
+        <Logo />
+      </div>
+
+      <a href="#" className="header__link pl-5 pr-5">
+        <ProfileIcon type="secondary" />
+        <p className="text text_type_main-default text_color_inactive pr-2 pl-2">
+          {" "}
+          Личный кабинет
+        </p>
+      </a>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
