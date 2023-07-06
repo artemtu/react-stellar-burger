229286754e9350ff7120072331a33d64522cf6fe
Artem Tulupov <artemtu94@gmail.com>
2023-07-06 19:26:23 +0400

    feature: add constructor for bun (top & bottom)

--- src/components/burgerconstructor/constructorelement.jsx
+++ src/components/burgerconstructor/constructorelement.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components'
 import constructor__scroll from './constructorelement.css'
+import { data } from '../../utils/data';
+
+const bun = data.find((item) => item._id === '60666c42cc7b410027a1a9b1');
+
+const ingredients = data.filter((item) => item.type !== 'bun');
 
 
 
 
 function Constructorelement() {
+
         return (
           <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
             <ConstructorElement
               type="top"
               isLocked={true}
-              text="Краторная булка N-200i (верх)"
-              price={200}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+              text={bun.name + '(верх)'}
+              price={bun.price}
+              thumbnail={bun.image_mobile}
             />
             <div className='constructor__scroll custom-scroll'>
 
@@ -22,43 +28,14 @@ function Constructorelement() {
               price={50}
               thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
             />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-                       <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
+
             </div>
             <ConstructorElement
               type="bottom"
               isLocked={true}
-              text="Краторная булка N-200i (низ)"
-              price={200}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+              text={bun.name + '(низ)'}
+              price={bun.price}
+              thumbnail={bun.image_mobile}
             />
           </div>
         )
