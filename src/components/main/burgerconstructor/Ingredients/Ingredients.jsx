f6685fef980c24cee7992f8ed19a7ca34749a03c
Artem Tulupov <artemtu94@gmail.com>
2023-07-14 14:44:53 +0400

    bugfix: rename ingredients style

--- src/components/main/burgerconstructor/Ingredients/Ingredients.jsx
+++ src/components/main/burgerconstructor/Ingredients/Ingredients.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import { data } from "../../../../utils/data";
-import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import ingredientZ from './ingredients.css'
-
-
-
+import {
+  ConstructorElement,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import list from "./ingredients.css";
 
 const ingredients = data.filter((item) => item.type !== "bun");
 
 function Ingredients() {
   return (
-    <div className="ingredientZ custom-scroll" style={{gap:'10px'}}>
-      {ingredients.map((item) => (  
+    <div className="list custom-scroll" style={{ gap: "10px" }}>
+      {ingredients.map((item) => (
         <div>
-          <DragIcon/>
-
+          <DragIcon />
           <ConstructorElement
-          className='items'
+            className="items"
             key={item._id}
             text={item.name}
             price={item.price}
             thumbnail={item.image}
           />
         </div>
-      ))
-} 
+      ))}
     </div>
   );
 }
 
 export default Ingredients;
-
-
