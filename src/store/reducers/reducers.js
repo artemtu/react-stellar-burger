bbf9945f5821134f7277af7b644c9d8731474923
Artem Tulupov <artemtu94@gmail.com>
2023-07-30 18:56:38 +0400

    bugfix

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -5,14 +5,13 @@ import constructorReducer from './constructorReducer';
 import ingredientOpenReducer from './ingredientOpenReducer';
 import orderDetailReducer from './orderDetailsReducer';
 
-// Используйте combineReducers для объединения всех ваших редьюсеров в один корневой редьюсер
+
 const rootReducer = combineReducers({
   mainData: ingredientReducer,
   constructorBurger: constructorReducer,
   ingredient: ingredientOpenReducer,
   orderDetails: orderDetailReducer,
   
-  // Добавьте другие редьюсеры здесь, если у вас их больше
 });
 
 export default rootReducer;
