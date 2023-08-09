93d8646a09a8d788e3253d42682ad8aa615595c0
Artem Tulupov <artemtu94@gmail.com>
2023-08-09 16:39:37 +0400

    feature: register page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -9,12 +9,15 @@ import PropTypes from "prop-types";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../../pages/home/home";
 import Login from "../../pages/login/login";
+import Register from "../../pages/register/register";
 
 function App() {
   return (
     <Router>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+
 
     </Router>
   );
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,54 +1,50 @@
 import React from "react";
 import Header from "../../components/app-header/app-header";
+import styles from "./register.module.css";
 import { PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import { EmailInput } from "@ya.praktikum/react-developer-burger-ui-components";
-import styles from "./login.module.css";
 import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Link } from "react-router-dom";
 
-function Login() {
+function Register() {
   const [value, setValue] = React.useState();
-
   return (
     <>
       <Header />
-      <div className={styles.login}>
+      <div className={styles.register}>
         <h3 className={`${styles.textEnter} text text_type_main-medium`}>
-          Вход
+          Регистрация
         </h3>
-        <EmailInput
-          extraClass="mt-6"
+        <Input
+          type={"text"}
+          placeholder={"Имя"}
           value={value}
-          // onChange={onChange}
-        />
-        <PasswordInput
-          // onChange={onChange}
-          value={value}
-          name={"password"}
+          name={"name"}
+          error={false}
+          errorText={"Ошибка"}
+          size={"default"}
           extraClass="mt-6"
         />
+
+        <EmailInput extraClass="mt-6" value={value} />
+        <PasswordInput value={value} name={"password"} extraClass="mt-6" />
         <Button
           htmlType="button"
           type="primary"
           size="large"
           extraClass={`${styles.button} mt-6`}
         >
-          Войти
+          Зарегистрироваться
         </Button>
         <p className="text text_type_main-small text_color_inactive mt-20">
           {" "}
-          Вы - новый пользователь? <Link to="#">Зарегистрироваться </Link>{" "}
+          Уже зарегистрированы? <Link to="#">Войти </Link>{" "}
         </p>
-        <p className="text text_type_main-small text_color_inactive mt-4">
-          Забыли пароль?{" "}
-          <Link className="text text_type_main-small" to ='#'>
-            Восстановить пароль
-          </Link>
-        </p>
+ 
       </div>
     </>
   );
 }
 
-export default Login;
+export default Register;
