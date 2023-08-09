40b8807f3e2efed97ddfc901832e2824fd586b15
Artem Tulupov <artemtu94@gmail.com>
2023-08-09 16:25:02 +0400

    feature: login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/app-header/app-header";
+import { PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
+import { EmailInput } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./login.module.css";
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [value, setValue] = React.useState();
+
   return (
-    <h1>кек</h1>
-  )
+    <>
+      <Header />
+      <div className={styles.login}>
+        <h3 className={`${styles.textEnter} text text_type_main-medium`}>
+          Вход
+        </h3>
+        <EmailInput
+          extraClass="mt-6"
+          value={value}
+          // onChange={onChange}
+        />
+        <PasswordInput
+          // onChange={onChange}
+          value={value}
+          name={"password"}
+          extraClass="mt-6"
+        />
+        <Button
+          htmlType="button"
+          type="primary"
+          size="large"
+          extraClass={`${styles.button} mt-6`}
+        >
+          Войти
+        </Button>
+        <p className="text text_type_main-small text_color_inactive mt-20">
+          {" "}
+          Вы - новый пользователь? <Link to="/">Зарегистрироваться </Link>{" "}
+        </p>
+        <p className="text text_type_main-small text_color_inactive mt-4">
+          Забыли пароль?{" "}
+          <Link className="text text_type_main-small" to ='/'>
+            Восстановить пароль
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
