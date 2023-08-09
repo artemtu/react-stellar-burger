863c60e31ece665a69e91f240cc720884f08773d
Artem Tulupov <artemtu94@gmail.com>
2023-08-09 16:25:52 +0400

    feature: login upgrade in app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,11 +8,14 @@ import OrderDetails from "../modal/order-details/orderdetails";
 import PropTypes from "prop-types";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../../pages/home/home";
+import Login from "../../pages/login/login";
 
 function App() {
   return (
     <Router>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+
     </Router>
   );
 }
