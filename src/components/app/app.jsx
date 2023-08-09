d24023e78f2fab5e35a83cbfe77c140656fa3e81
Artem Tulupov <artemtu94@gmail.com>
2023-08-09 17:35:20 +0400

    feature: 404 & reset-password

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,19 +6,32 @@ import Modal from "../modal/modal";
 import IngredientDetails from "../modal/ingredient-details/ingredient-details";
 import OrderDetails from "../modal/order-details/orderdetails";
 import PropTypes from "prop-types";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 import Home from "../../pages/home/home";
 import Login from "../../pages/login/login";
 import Register from "../../pages/register/register";
 import ForgotPassword from "../../pages/forgot-password/forgot-password";
+import ResetPassword from "../../pages/reset-password/reset-password";
+import NotFound404 from "../../pages/not-found-404/not-found-404";
+
+
 
 function App() {
   return (
     <Router>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route exact path="/reset-password" component={ResetPassword} />
+        <Route component={NotFound404} /> 
+
+        </Switch>
+        
+    
+
+     
 
 
     </Router>
---
+++ src/pages/reset-password/reset-password.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Header from "../../components/app-header/app-header";
+import styles from "./reset-password.module.css";
+import { PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
+
+function ResetPassword() {
+  const [value, setValue] = React.useState();
+  return (
+    <>
+      <Header />
+      <div className={styles.reset}>
+        <h3 className={`${styles.textEnter} text text_type_main-medium`}>
+          Восстановление пароля
+        </h3>
+        <PasswordInput value={value} name={"password"} extraClass="mt-6" />
+        <Input
+          type={"text"}
+          placeholder={"Введите код из письма"}
+          value={value}
+          name={"name"}
+          error={false}
+          errorText={"Ошибка"}
+          size={"default"}
+          extraClass="mt-6"
+        />
+        <Button
+          htmlType="button"
+          type="primary"
+          size="large"
+          extraClass={`${styles.button} mt-6`}
+        >
+          Сохранить
+        </Button>
+        <p className="text text_type_main-small text_color_inactive mt-20">
+          {" "}
+          Вспомнили пароль? <Link to="#">Войти </Link>{" "}
+        </p>
+ 
+      </div>
+    </>
+  );
+}
+
+export default ResetPassword;
