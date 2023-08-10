2e186ab05f4a1a47fa44d3aee8a3f4abc3765f6b
Artem Tulupov <artemtu94@gmail.com>
2023-08-10 14:33:35 +0400

    feture

--- src/store/actions/register-user.js
+++ src/store/actions/register-user.js
@@ -2,7 +2,6 @@ import { config } from "../../components/api/api";
 import { checkResponse } from "../../components/api/api";
 import { REGISTER_USER } from "./actions";
 
-
 export const getUserRegistration = (profileData) => {
   return {
     type: REGISTER_USER,
@@ -10,16 +9,17 @@ export const getUserRegistration = (profileData) => {
   };
 };
 
-
 export const postUser = (registration) => (dispatch) => {
   return fetch(`${config.baseUrl}/auth/register`, {
     method: "POST",
     headers: config.headers,
     body: JSON.stringify(registration),
-  }).then(checkResponse) 
+  })
+    .then(checkResponse)
     .then((data) => {
-     dispatch(getUserRegistration(data));
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("refreshToken", data.refreshToken);
+      dispatch(getUserRegistration(data));
     })
-    .catch(console.error) 
+    .catch(console.error);
 };
-
