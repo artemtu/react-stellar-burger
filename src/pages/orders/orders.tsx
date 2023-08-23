db484518d7d4788d83dde5fefe50e133a1a67ad1
Artem Tulupov <artemtu94@gmail.com>
2023-08-23 15:32:25 +0400

    feature: orders to tsx

--- src/pages/orders/orders.tsx
+++ src/pages/orders/orders.tsx
@@ -12,8 +12,8 @@ function ProfileOrders() {
     <h1>Страница в разработке</h1>
     <div className={styles.LeftMenu}>
         <Link className={`text text_type_main-medium ${location.pathname === '/orders' ? '' : 'text_color_inactive'}`} to='/profile' style={{ textDecoration: 'none' }} >Профиль</Link>
-        <Link className={`text text_type_main-medium `}  style={{ textDecoration: 'none' }}>История заказов</Link>
-        <Link className={`text text_type_main-medium ${location.pathname === '/orders' ? '' : 'text_color_inactive'}`} style={{ textDecoration: 'none' }}>Выход</Link>
+        <Link className={`text text_type_main-medium `}  style={{ textDecoration: 'none' }} to='/profile/orders'>История заказов</Link>
+        <Link className={`text text_type_main-medium ${location.pathname === '/orders' ? '' : 'text_color_inactive'}`} style={{ textDecoration: 'none' }} to='/logout'>Выход</Link>
         <p className='text text_type_main-small'>В этом разделе вы можете изменить свои персональные данные</p>
       </div>
     </>
