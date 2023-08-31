c72c7be8a1cea7f0270a496aca104f825ca4bd1d
Artem Tulupov <artemtu94@gmail.com>
2023-08-31 16:54:40 +0400

    feature: add-remove to ts

---
+++ src/store/actions/add-remove.ts
@@ -0,0 +1,28 @@
+import {
+  ADD_INGREDIENT,
+  REMOVE_INGREDIENT,
+  CHANGE_INGREDIENT,
+} from "./actions";
+import { IData } from "./fetch-data";
+
+export const addIngredient = (ingredient: IData) => {
+  return {
+    type: typeof ADD_INGREDIENT,
+    payload: ingredient,
+  };
+};
+
+// Экшен для удаления ингредиента по его ID
+export const removeIngredient = (ingredientId: string) => {
+  return {
+    type: typeof REMOVE_INGREDIENT,
+    payload: ingredientId,
+  };
+};
+
+export const changeIngredient = (ingredient: IData) => {
+  return {
+    type: typeof CHANGE_INGREDIENT,
+    payload: ingredient,
+  };
+};
