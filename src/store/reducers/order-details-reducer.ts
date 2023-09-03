1ca81da15651941319f12acfa67bc088ee92af6f
Artem Tulupov <artemtu94@gmail.com>
2023-09-03 13:47:10 +0400

    feature: order-detais reducer to ts

--- src/store/reducers/order-details-reducer.ts
+++ src/store/reducers/order-details-reducer.ts
@@ -1,10 +1,10 @@
 import { GET_ORDER_NUMBER } from "../actions/actions";
+import { ActionTypes } from "../types";
 
-const initialState = {
+const initialState = {};
 
-};
-
-const orderDetailReducer = (state = initialState, action) => {
+const orderDetailReducer = (state = initialState, action: ActionTypes) => {
+  //@ts-ignore
   switch (action.type) {
     case GET_ORDER_NUMBER:
       return {
