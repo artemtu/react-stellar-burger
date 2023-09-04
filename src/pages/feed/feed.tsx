59066f0cd2995bd1bf7328013e93ffd339ba9933
Artem Tulupov <artemtu94@gmail.com>
2023-09-04 19:50:13 +0400

    feature: add section to feed with red border img

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import styles from "./feed.module.css";
-
+import { FormattedDate } from "@ya.praktikum/react-developer-burger-ui-components";
 
 function Feed() {
-
- 
+  const today = new Date();
+  const yesterday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 1,
+    today.getHours(),
+    today.getMinutes() - 1,
+    0
+  );
 
   return (
-    <h1 >ERE </h1>
-  )
+    <section className={`${styles.menuBar} mt-10`}>
+      <h1 className="text text_type_main-large">Лента заказов</h1>
+      <div>
+        <div className={`${styles.numberDate} mt-6`}>
+          <p className="text text_type_digits-default"> #03402304</p>
+          <FormattedDate
+            date={yesterday}
+            className="text text_type_main-default text_color_inactive"
+          />
+        </div>
+        <p className="text text_type_main-medium mt-6">
+          Death Star Starship Main Burger
+        </p>
+        <div className={styles.container}>
+          <div className={styles.ingredients}>
+            <img
+              src="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+              alt=""
+              className={styles.kek}
+            />
+            
+          </div>
+          
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Feed;
