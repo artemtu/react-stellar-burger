e1d7cef19aa59282ccc62f952858eac893a23ac9
Artem Tulupov <artemtu94@gmail.com>
2023-09-06 19:09:25 +0400

    fe

---
+++ src/store/reducers/feed-reducer.js
@@ -0,0 +1,21 @@
+import { GET_FEED } from "../actions/actions";
+import { ActionTypes } from "../types";
+
+const initialState = {
+};
+
+const feedReducer = (state = initialState, action) => {
+  //@ts-ignore
+  switch (action.type) {
+    case GET_FEED:
+      return {
+        ...state,
+        getFeed: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default feedReducer;
