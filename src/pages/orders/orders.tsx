c49433f774e40b3cb4c467c9e120e181d8f3992d
Artem Tulupov <artemtu94@gmail.com>
2023-09-09 14:00:16 +0400

    feature: implement ord hist to orders

--- src/pages/orders/orders.tsx
+++ src/pages/orders/orders.tsx
@@ -3,17 +3,20 @@ import Header from '../../components/app-header/app-header'
 import { Link } from "react-router-dom";
 import styles from './orders.module.css'
 import {useLocation} from "react-router-dom";
+import OrdersHistory from '../orders-history/orders-history';
 
 function ProfileOrders() {
     const location = useLocation();
   return (
     <>
-    <h1>Страница в разработке</h1>
     <div className={styles.LeftMenu}>
         <Link className={`text text_type_main-medium ${location.pathname === '/orders' ? '' : 'text_color_inactive'}`} to='/profile' style={{ textDecoration: 'none' }} >Профиль</Link>
         <Link className={`text text_type_main-medium `}  style={{ textDecoration: 'none' }} to='/profile/orders'>История заказов</Link>
         <Link className={`text text_type_main-medium ${location.pathname === '/orders' ? '' : 'text_color_inactive'}`} style={{ textDecoration: 'none' }} to='/logout'>Выход</Link>
         <p className='text text_type_main-small'>В этом разделе вы можете изменить свои персональные данные</p>
+      </div>
+      <div>
+        <OrdersHistory/>
       </div>
     </>
   )
