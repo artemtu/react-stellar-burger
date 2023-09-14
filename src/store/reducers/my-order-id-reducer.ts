cca31573186d3a47e9181b023bc7f576234783cb
Artem Tulupov <artemtu94@gmail.com>
2023-09-14 15:00:46 +0400

    feature: order-id reducer types

---
+++ src/store/reducers/my-order-id-reducer.ts
@@ -0,0 +1,15 @@
+import { SET_MY_ORDER_ID } from "../actions/actions";
+import { ActionTypes } from "../types";
+
+const initialState = {
+  myOrderId: null,
+};
+
+export const myOrderIdReducer = (state = initialState, action: ActionTypes) => {
+  switch (action.type) {
+    case SET_MY_ORDER_ID:
+      return { ...state, myOrderId: action.payload };
+    default:
+      return state;
+  }
+};
