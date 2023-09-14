63bcb1c2685bbe02a5f9c6e48ec8dcf416d1f4e2
Artem Tulupov <artemtu94@gmail.com>
2023-09-14 15:49:39 +0400

    feature images-line useappsel

--- src/components/images-line/images-line.tsx
+++ src/components/images-line/images-line.tsx
@@ -4,29 +4,29 @@ import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useMemo } from "react";
+import { useAppSelector } from "../../store/types";
 
-function ImagesIngredients({images}:any) {
+function ImagesIngredients({ images }: any) {
   //@ts-ignore
-  const ingredients = useSelector((state) => state.getFeed?.getFeed?.orders);
-    //@ts-ignore
-  const AllIngredients = useSelector((state) => state.mainData.data);
+  const ingredients = useAppSelector((state) => state.getFeed?.getFeed?.orders);
+
+  const AllIngredients = useAppSelector((state) => state.mainData.data);
 
   const displayedImages = images ? images.slice(0, 5) : [];
   const hiddenCount = images ? images.length - 5 : 0;
 
   return (
     <div className={styles.test}>
-      {displayedImages.map((item:any, index:number) => (
+      {displayedImages.map((item: any, index: number) => (
         <div key={index} className={styles.ingredients}>
           <img src={item} alt="" className={styles.ingredientPosition} />
         </div>
       ))}
       {hiddenCount > 0 && (
-          <div className={styles.additionalIngredients}>+{hiddenCount}</div>
+        <div className={styles.additionalIngredients}>+{hiddenCount}</div>
       )}
     </div>
   );
 }
 
 export default ImagesIngredients;
-
