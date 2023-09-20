015b64057f8633fa0d1b6c0407bcea71063e9f45
Artem Tulupov <artemtu94@gmail.com>
2023-09-20 21:35:24 +0400

    feature: get-profile-reducer test

---
+++ src/store/reducers/get-profile-info-reducer.test.js
@@ -0,0 +1,43 @@
+import { GET_PROFILE_INFO } from "../actions/actions";
+import profileInfoReducer from "./get-profile-info-reducer";
+
+const initialState = {
+  profileInfo: {
+    user: {
+      name: "",
+      email: "",
+    },
+  },
+};
+
+// Тест
+describe("get-profile-info-reducer", () => {
+  it("should return the initial state of get-profile-info-reducer", () => {
+    expect(profileInfoReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should handle GET_PROFILE_INFO action", () => {
+    const action = {
+      type: GET_PROFILE_INFO,
+      payload: {
+        success: true,
+        user: {
+          name: "John",
+          email: "john@example.com",
+        },
+      },
+    };
+
+    const expectedState = {
+      profileInfo: {
+        success: true,
+        user: {
+          name: "John",
+          email: "john@example.com",
+        },
+      },
+    };
+
+    expect(profileInfoReducer(initialState, action)).toEqual(expectedState);
+  });
+});
