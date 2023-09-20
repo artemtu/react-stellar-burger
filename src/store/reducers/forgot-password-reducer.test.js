051b2bec4b244e660778961c6954b71ca3ad5d8d
Artem Tulupov <artemtu94@gmail.com>
2023-09-20 21:20:16 +0400

    feature: forgot-password test

---
+++ src/store/reducers/forgot-password-reducer.test.js
@@ -0,0 +1,29 @@
+import { FORGOT_PASSWORD } from "../actions/actions";
+import forgotPasswordReducer from "./forgot-password-reducer";
+
+describe("forgot-password-reducer", () => {
+  it("should return the initial state of forgotPasswordReducer", () => {
+    expect(forgotPasswordReducer(undefined, {})).toEqual({
+      isSuccess: false,
+    });
+  });
+  it("should handle FORGOT_PASSWORD action", () => {
+    const action = {
+      type: FORGOT_PASSWORD,
+      payload: {
+        success: true,
+        message: "Reset email sent",
+      },
+    };
+
+    const expectedState = {
+      isSuccess: true,
+      forgotPassword: {
+        success: true,
+        message: "Reset email sent",
+      },
+    };
+
+    expect(forgotPasswordReducer(undefined, action)).toEqual(expectedState);
+  });
+});
