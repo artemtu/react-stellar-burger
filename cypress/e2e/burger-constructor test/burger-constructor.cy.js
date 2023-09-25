0e00c638b74f93298c7a79e2226fdeee3a73bd9c
Artem Tulupov <artemtu94@gmail.com>
2023-09-25 16:29:59 +0400

    feature: change ingredients place

--- cypress/e2e/burger-constructor test/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor test/burger-constructor.cy.js
@@ -2,7 +2,7 @@ describe("burger constructor test", () => {
   it("open main page", () => {
     cy.visit("http://localhost:3001");
   });
-  it("dnd of the ingredient", () => {
+  it("dnd for ingredients", () => {
     cy.visit("http://localhost:3001");
     cy.get('div[draggable="true"]').first().drag(".main_burgerBar__sOxnh");
     cy.get('div[draggable="true"]')
@@ -19,6 +19,17 @@ describe("burger constructor test", () => {
       .eq(11)
       .then(($elem) => {
         cy.wrap($elem).drag(".main_burgerBar__sOxnh");
+      });
+    cy.get(".ingredients_constructorBurger__St8Eo")
+      .eq(1)
+      .then(($elem) => {
+        cy.get(".ingredients_constructorBurger__St8Eo")
+          .eq(0)
+          .then(($firstElem) => {
+            $elem
+              .get(0)
+              .parentNode.insertBefore($elem.get(0), $firstElem.get(0));
+          });
       });
   });
 });
