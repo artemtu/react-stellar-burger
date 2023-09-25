0a68384da458f143ce2e2108699b47c10b38db9a
Artem Tulupov <artemtu94@gmail.com>
2023-09-25 16:09:08 +0400

    feature: added ingredients to constructor burger

--- cypress/e2e/burger-constructor test/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor test/burger-constructor.cy.js
@@ -5,5 +5,20 @@ describe("burger constructor test", () => {
   it("dnd of the ingredient", () => {
     cy.visit("http://localhost:3001");
     cy.get('div[draggable="true"]').first().drag(".main_burgerBar__sOxnh");
+    cy.get('div[draggable="true"]')
+      .eq(5)
+      .then(($elem) => {
+        cy.wrap($elem).drag(".main_burgerBar__sOxnh");
+      });
+    cy.get('div[draggable="true"]')
+      .eq(10)
+      .then(($elem) => {
+        cy.wrap($elem).drag(".main_burgerBar__sOxnh");
+      });
+    cy.get('div[draggable="true"]')
+      .eq(11)
+      .then(($elem) => {
+        cy.wrap($elem).drag(".main_burgerBar__sOxnh");
+      });
   });
 });
