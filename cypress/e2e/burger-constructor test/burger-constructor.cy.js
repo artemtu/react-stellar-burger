0785762cf9813ec90415e3a1b08b29aa941af414
Artem Tulupov <artemtu94@gmail.com>
2023-09-25 16:02:22 +0400

    feature: drug elemenet to bun.

--- cypress/e2e/burger-constructor test/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor test/burger-constructor.cy.js
@@ -2,4 +2,8 @@ describe("burger constructor test", () => {
   it("open main page", () => {
     cy.visit("http://localhost:3001");
   });
+  it("dnd of the ingredient", () => {
+    cy.visit("http://localhost:3001");
+    cy.get('div[draggable="true"]').first().drag(".main_burgerBar__sOxnh");
+  });
 });
