63bde5933d0d7fcabe15396ed4f71b4edb362559
Artem Tulupov <artemtu94@gmail.com>
2023-09-26 15:30:11 +0400

    bugfix: ingredient red test with const

--- src/store/reducers/ingredient-reducer.test.js
+++ src/store/reducers/ingredient-reducer.test.js
@@ -1,6 +1,7 @@
 import { GET_INGREDIENTS } from "../actions/actions";
 import ingredientReducer from "./ingredient-reducer";
 import { initialState } from "./ingredient-reducer";
+import { bunObject } from "./constants-reducers";
 
 
 describe("ingredientReducer", () => {
@@ -11,43 +12,11 @@ describe("ingredientReducer", () => {
   it("should handle GET_INGREDIENTS action ", () => {
     const action = {
       type: GET_INGREDIENTS,
-      payload: [
-        {
-          _id: "11ааfdsf",
-          name: "Какая-то булка",
-          type: "bun",
-          proteins: 44,
-          fat: 26,
-          carbohydrates: 85,
-          calories: 643,
-          price: 988,
-          image: "https://code.s3.yandex.net/react/code/bun-01.png",
-          image_mobile:
-            "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-          image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-          __v: 0,
-        },
-      ],
+      payload: [bunObject],
     };
 
     const expectedState = {
-      data: [
-        {
-          _id: "11ааfdsf",
-          name: "Какая-то булка",
-          type: "bun",
-          proteins: 44,
-          fat: 26,
-          carbohydrates: 85,
-          calories: 643,
-          price: 988,
-          image: "https://code.s3.yandex.net/react/code/bun-01.png",
-          image_mobile:
-            "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-          image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-          __v: 0,
-        },
-      ],
+      data: [bunObject],
 
       isLoading: false,
     };
