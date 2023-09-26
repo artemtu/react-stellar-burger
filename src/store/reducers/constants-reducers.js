017fd2b911506d433e96ccf5fc87a5a3732bcf8a
Artem Tulupov <artemtu94@gmail.com>
2023-09-26 15:28:48 +0400

    bugfix: ingred open red to consts

--- src/store/reducers/constants-reducers.js
+++ src/store/reducers/constants-reducers.js
@@ -5,3 +5,18 @@ export const profileInfo = {
     email: "john@example.com",
   },
 };
+
+export const bunObject = {
+  _id: "11ааfdsf",
+  name: "Какая-то булка",
+  type: "bun",
+  proteins: 44,
+  fat: 26,
+  carbohydrates: 85,
+  calories: 643,
+  price: 988,
+  image: "https://code.s3.yandex.net/react/code/bun-01.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+  __v: 0,
+};
--- src/store/reducers/ingredient-open-reducer.test.js
+++ src/store/reducers/ingredient-open-reducer.test.js
@@ -5,6 +5,7 @@ import {
 
 import ingredientOpenReducer from "./ingredient-open-reducer";
 import { initialState } from "./ingredient-open-reducer";
+import { bunObject } from "./constants-reducers";
 
 
 
@@ -15,43 +16,11 @@ describe("ingredientOpenReducer", () => {
   it("should handle GET_INFO_OPEN_INGREDIENT_MODAL action ", () => {
     const action = {
       type: GET_INFO_OPEN_INGREDIENT_MODAL,
-      payload: [
-        {
-          _id: "11ааfdsf",
-          name: "Какая-то булка",
-          type: "bun",
-          proteins: 44,
-          fat: 26,
-          carbohydrates: 85,
-          calories: 643,
-          price: 988,
-          image: "https://code.s3.yandex.net/react/code/bun-01.png",
-          image_mobile:
-            "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-          image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-          __v: 0,
-        },
-      ],
+      payload: [bunObject],
     };
 
     const expectedState = {
-      ingredient: [
-        {
-          _id: "11ааfdsf",
-          name: "Какая-то булка",
-          type: "bun",
-          proteins: 44,
-          fat: 26,
-          carbohydrates: 85,
-          calories: 643,
-          price: 988,
-          image: "https://code.s3.yandex.net/react/code/bun-01.png",
-          image_mobile:
-            "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-          image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-          __v: 0,
-        },
-      ],
+      ingredient: [bunObject],
     };
 
     expect(ingredientOpenReducer(initialState, action)).toEqual(expectedState);
@@ -60,23 +29,7 @@ describe("ingredientOpenReducer", () => {
   it("should handle CLEAR_INGREDIENT_OPEN action ", () => {
     const action = {
       type: CLEAR_INGREDIENT_OPEN,
-      payload: [
-        {
-          _id: "11ааfdsf",
-          name: "Какая-то булка",
-          type: "bun",
-          proteins: 44,
-          fat: 26,
-          carbohydrates: 85,
-          calories: 643,
-          price: 988,
-          image: "https://code.s3.yandex.net/react/code/bun-01.png",
-          image_mobile:
-            "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-          image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-          __v: 0,
-        },
-      ],
+      payload: [bunObject],
     };
 
     const expectedState = {
