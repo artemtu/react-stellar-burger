4f3fbf01dc281189aca113548401a825714151e4
Artem Tulupov <artemtu94@gmail.com>
2023-09-26 18:12:47 +0400

    feature: change to tsx feed id page & index

--- src/pages/feed-id-page/feed-id-page.tsx
+++ src/pages/feed-id-page/feed-id-page.tsx
@@ -3,14 +3,11 @@ import { useParams } from "react-router-dom";
 import styles from "./feed-id-page.module.css";
 import { useAppDispatch, useAppSelector } from "../../store/types";
 import FeedPage from "../../components/feed-id-modal/feed-id-modal";
-// import { fetchFeed } from "../../store/actions/feed-all-orders";
 
 function FeedPageTest() {
   const dispatch = useAppDispatch();
   const { id } = useParams();
   const feedData = useAppSelector((state) => state.getFeed.getFeed);
-
- 
 
   useEffect(() => {
     dispatch({ type: "WS_FEED_INIT" });
