cbaf2d99e326c8aea24d350950cc7ff317dc8d75
Artem Tulupov <artemtu94@gmail.com>
2023-09-26 12:17:14 +0400

    bugfix: drop except code in Home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import Header from "../../components/app-header/app-header";
 import Main from "../../components/main/main";
 import Modal from "../../components/modal/modal";
 import IngredientDetails from "../../components/modal/ingredient-details/ingredient-details";
 import OrderDetails from "../../components/modal/order-details/orderdetails";
 import { useState } from "react";
 import styles from "./home.module.css";
-import { useDispatch } from "react-redux";
-import { useAppDispatch } from "../../store/types";
 
 function Home() {
-  const dispatch = useAppDispatch();
-
-  // const [isFeedIdModal, setIsFeedIdModal] = useState({ false: true, id: 1 });
-
   const [isOrderModal, setOrderModal] = useState({
     open: false,
   });
@@ -25,21 +17,14 @@ function Home() {
   function closeModal() {
     setOrderModal({ open: false });
     setIngredientModal({ open: false, id: 1 });
-    // setIsFeedIdModal({ open: false, id: 1 });
   }
 
   function openModal() {
     setOrderModal({ open: true });
   }
 
-  // function openFeedId() {
-  //   // console.log('я здест');
-  //   setIsFeedIdModal({ open: true });
-  // }
-
   return (
     <div className={styles.app}>
-      {/* <SomeComponent /> */}
       <Main
         setOrderModal={setOrderModal}
         setIngredientModal={setIngredientModal}
@@ -56,11 +41,6 @@ function Home() {
           <IngredientDetails id={isIngredientModal.id} />
         </Modal>
       )}
-      {/* {isFeedIdModal.open && (
-        <Modal closeModal={closeModal}>
-          <FeedPage id={isFeedIdModal.id} />
-        </Modal>
-      )} */}
     </div>
   );
 }
