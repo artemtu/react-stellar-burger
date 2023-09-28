916629c6f64478bf2ad3ac5935c99acaa39e8f4a
Artem Tulupov <artemtu94@gmail.com>
2023-09-28 18:42:22 +0400

    bugfix: feed id page socket connect new

--- src/pages/feed-id-page/feed-id-page.tsx
+++ src/pages/feed-id-page/feed-id-page.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import styles from "./feed-id-page.module.css";
 import { useAppDispatch, useAppSelector } from "../../store/types";
 import FeedPage from "../../components/feed-id-modal/feed-id-modal";
+import { WSS_OPEN, WSS_CLOSE } from "../../store/actions/actions";
+import { socketUrl } from "../../store/socketMiddleware";
+import { GET_FEED } from "../../store/actions/actions";
 
 function FeedPageTest() {
   const dispatch = useAppDispatch();
@@ -11,10 +14,27 @@ function FeedPageTest() {
   const feedData = useAppSelector((state) => state.getFeed.getFeed);
 
   useEffect(() => {
-    dispatch({ type: "WS_FEED_INIT" });
+    dispatch({
+      type: WSS_OPEN,
+      meta: {
+        socket: {
+          event: "INIT",
+          uri: socketUrl + "/all",
+          actionTypes: { message: GET_FEED },
+        },
+      },
+    });
 
     return () => {
-      dispatch({ type: "WS_FEED_CLOSE" });
+      dispatch({
+        type: WSS_CLOSE,
+        meta: {
+          socket: {
+            event: "CLOSE",
+            uri: socketUrl + "/all",
+          },
+        },
+      });
     };
   }, [dispatch]);
 
